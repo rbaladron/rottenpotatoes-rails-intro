0eb61767e790e9902b0e9cb7fd09890ac84e5ec0
Roberto Baladrón Pardo <rbaladron@gmail.com>
2018-09-10 10:48:03 +0000

    part 1, 2 ,3  complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -5,13 +5,36 @@ class MoviesController < ApplicationController
   end
 
   def show
-    id = params[:id] # retrieve movie ID from URI route
-    @movie = Movie.find(id) # look up movie by unique ID
-    # will render app/views/movies/show.<extension> by default
+     #id = params [:id]           # retrieve movie ID from URI route 
+     #@movie = Movie.find(id)     # look up movie by unique ID
+     # whill render app/views/movies/show.html.haml by default
+     @movie = Movie.find(params[:id])
   end
 
   def index
-    @movies = Movie.all
+    if(!params.has_key?(:sort) && !params.has_key?(:ratings))
+      if(session.has_key?(:sort) || session.has_key?(:ratings))
+        redirect_to movies_path(:sort=>session[:sort], :ratings=>session[:ratings])
+      end
+    end
+    @sort = params.has_key?(:sort) ? (session[:sort] = params[:sort]) : session[:sort]
+    @all_ratings = Movie.all_ratings.keys
+    @ratings = params[:ratings]
+    if(@ratings != nil)
+      ratings = @ratings.keys
+      session[:ratings] = @ratings
+    else
+      if(!params.has_key?(:commit) && !params.has_key?(:sort))
+        ratings = Movie.all_ratings.keys
+        session[:ratings] = Movie.all_ratings
+      else
+        ratings = session[:ratings].keys
+      end
+    end
+    #@movies = Movie.order(@sort).find_all_by_rating(ratings) ==> no funciona en Rails 5
+    @movies = Movie.order(@sort).where(rating: ratings)
+    @mark  = ratings
+
   end
 
   def new
@@ -41,5 +64,23 @@ class MoviesController < ApplicationController
     flash[:notice] = "Movie '#{@movie.title}' deleted."
     redirect_to movies_path
   end
+  
+  def find_class(header)
+    params[:sort] == header ? 'hilite' : nil
+  end
+  helper_method :find_class
+  
+  private
+    def movie_params #:title, :rating, :description, :release_date
+      params.require(:movie).permit(:title, :rating, :description, :release_date )
+    end
+    
+    def check
+      if params[:ratings]
+        params[:ratings].keys
+      else
+        @all_ratings
+      end
+    end
 
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,10 @@
 class Movie < ActiveRecord::Base
+  # attr_accessible :title, :rating, :description, :release_date, :director ==> No se usa en Rails 5
+  def self.all_ratings
+  	result = {}
+  	self.select(:rating).uniq.each do |movie|
+  		result[movie.rating] = 1
+  	end
+  	return result
+  end
 end
